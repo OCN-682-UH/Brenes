7076ee136c14cd09f0595227fc19f78c91df9d98
Brandon Brenes <brenesb@hawaii.edu>
2024-09-07 14:26:32 -1000

    read me file upload for week_02

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Brenes
+###### Last updated: 2024.09.07
+### **Purpose**: This repos will be used for OCN682: Data Science Fundamentals in R
+
+### **Contents**:
+#### The _Week#_ folders indicate the week that the subfolders _Data_, _Output_,   _Images_ and _Scripts_ were produced for their respective assignmnets.
+
+![Tip: if you put white borders on your photos it _almost_ makes it look like  you know what you're doing ;) ](/Users/brenesb/Desktop/Repositories/Brenes/Week_02/Images/IMG_5203.jpg)
--- Week_02/Scripts/week02_script.R
+++ Week_02/Scripts/week02_script.R
@@ -1,11 +1,18 @@
-#This is literally my first script. How exciting! 
-#The purpose of this script is to practice my skills in importing csv files (data)
-#The date is 2024.09.07
+#Description: 
+    #This is literally my first script. How exciting! 
+    #The purpose of this script is to practice my skills in importing csv files (data)
+    #This will be used for HW assignment of week 2
+    #The date is 2024.09.07
 #########################################
 
-##load libraries##
+##Load Libraries##
 library(tidyverse)
 library (here)
 
-### Read in data ###
+### Read in Data ###
 WeightData<-read_csv(here("Week_02","Data","weightdata.csv"))
+
+##Data Analysis##
+head(WeightData) #views top 6 lines of data set
+tail(WeightData) #views bottom 6 lines of data set
+view(WeightData) #views entire data set
