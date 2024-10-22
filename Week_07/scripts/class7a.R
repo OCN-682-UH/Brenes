3532efe80f12c6c5cb0142cb692f9075e95e3d92
Brandon Brenes <brenesb@hawaii.edu>
2024-10-21 17:40:53 -1000

    added week_08 folder

--- Week_07/scripts/class7a.R
+++ Week_07/scripts/class7a.R
@@ -14,6 +14,7 @@ library(mapdata)
 library(mapproj)
 library(readr)
 library(ggdogs)
+library(ggmap)
 
 ############################################################################################
 
